a6f78599cab283f48a0520c2884f3934a1b63d1c
Amandeep Kaur Sidhu <742135@student.tor.loc>
2019-04-04 10:49:26 -0400

    fifth commit

--- new.js
+++ new.js
@@ -1,3 +1,15 @@
+var user={
+    first_name:"css",
+    last_name:"Html",
+    age:23,/*from w w w.java 2s .c o m */
+    website:"java2s.com"
+};
+for(key in user){
+    console.log(key);
+}
+
+
+
 for(let i =0; i++; i<10){
     console.log(i);
 }
